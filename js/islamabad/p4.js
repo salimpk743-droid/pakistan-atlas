ab6b7318afd8ab79341e1aeb6106019da4b0f982
salimpk743-droid <salimpk743@gmail.com>
2026-09-02 23:12:48 +0400

    Update p4.js

--- js/islamabad/p4.js
+++ js/islamabad/p4.js
@@ -1,1 +1,2 @@
-window.ISB_PHOTOS=window.ISB_PHOTOS||[];ISB_PHOTOS.push({src:'data:image/jpeg;base64,'+_ISB.p4.join(''),alt:"Sector street",cap:"Sector road after rain, hills at the end of the street."});
+window.ISB_PHOTOS=window.ISB_PHOTOS||[];
+ISB_PHOTOS.push({src:"images/islamabad/04-sector-street.jpg",alt:"Sector street",cap:"Sector road after rain, hills at the end of the street."});
