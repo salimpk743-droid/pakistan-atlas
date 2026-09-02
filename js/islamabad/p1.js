1cd7877d7045058ab81c3fba1a71d0b63086275c
salimpk743-droid <salimpk743@gmail.com>
2026-09-02 23:11:59 +0400

    Update p1.js

--- js/islamabad/p1.js
+++ js/islamabad/p1.js
@@ -1,1 +1,2 @@
-window.ISB_PHOTOS=window.ISB_PHOTOS||[];ISB_PHOTOS.push({src:'data:image/jpeg;base64,'+_ISB.p1.join(''),alt:"Islamabad highway",cap:"Kashmir Highway / capital avenue, Parliament on the ridge."});
+window.ISB_PHOTOS=window.ISB_PHOTOS||[];
+ISB_PHOTOS.push({src:"images/islamabad/01-kashmir-highway.jpg",alt:"Islamabad highway",cap:"Kashmir Highway / capital avenue, Parliament on the ridge."});
