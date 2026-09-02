206337ffeda73c5bc11ef68f01d89218e0d16ae1
salimpk743-droid <salimpk743@gmail.com>
2026-09-02 23:13:15 +0400

    Update p6.js

--- js/islamabad/p6.js
+++ js/islamabad/p6.js
@@ -1,1 +1,2 @@
-window.ISB_PHOTOS=window.ISB_PHOTOS||[];ISB_PHOTOS.push({src:'data:image/jpeg;base64,'+_ISB.p6.join(''),alt:"Margalla night view",cap:"City lights from the ridge."});
+window.ISB_PHOTOS=window.ISB_PHOTOS||[];
+ISB_PHOTOS.push({src:"images/islamabad/06-margalla-view.jpg",alt:"Margalla night view",cap:"City lights from the ridge."});
