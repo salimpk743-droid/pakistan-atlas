2ecaa5cba224ab84c0cd17ab24f395601561da9c
salimpk743-droid <salimpk743@gmail.com>
2026-09-02 23:12:18 +0400

    Update p2.js

--- js/islamabad/p2.js
+++ js/islamabad/p2.js
@@ -1,1 +1,2 @@
-window.ISB_PHOTOS=window.ISB_PHOTOS||[];ISB_PHOTOS.push({src:'data:image/jpeg;base64,'+_ISB.p2.join(''),alt:"Faisal Mosque",cap:"Faisal Mosque against the Margallas."});
+window.ISB_PHOTOS=window.ISB_PHOTOS||[];
+ISB_PHOTOS.push({src:"images/islamabad/02-faisal-mosque.jpg",alt:"Faisal Mosque",cap:"Faisal Mosque against the Margallas."});
