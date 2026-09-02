d564f14c6dcc83a34271cbd0adab32014b4db688
salimpk743-droid <salimpk743@gmail.com>
2026-09-02 23:13:54 +0400

    Update load.js

--- js/islamabad/load.js
+++ js/islamabad/load.js
@@ -1,10 +1,6 @@
 (function(){
-  var spec={1:5,2:5,3:4,4:6,5:6,6:5,7:11};
-  var i,c;
+  var i;
   for(i=1;i<=7;i++){
-    for(c=0;c<spec[i];c++){
-      document.write('<script src="js/islamabad/p'+i+'-'+c+'.js"><\/script>');
-    }
     document.write('<script src="js/islamabad/p'+i+'.js"><\/script>');
   }
 })();
